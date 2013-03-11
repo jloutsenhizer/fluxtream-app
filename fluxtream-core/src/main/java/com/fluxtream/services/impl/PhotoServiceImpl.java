c866bcdc164d23287a1bb6c49a771407d9655efc
Chris Bartley <bartley@cmu.edu>
2013-03-11 11:10:44 -0400

    Removed obsolete TODOs

--- fluxtream-core/src/main/java/com/fluxtream/services/impl/PhotoServiceImpl.java
+++ fluxtream-core/src/main/java/com/fluxtream/services/impl/PhotoServiceImpl.java
@@ -291,7 +291,6 @@ public class PhotoServiceImpl implements PhotoService {
 
             final TimeInterval actualTimeInterval;
             if (timeInterval == null) {
-                // TODO: Not sure if this is correct for time zones...
                 actualTimeInterval = new TimeInterval(facet.start, facet.start, TimeUnit.DAY, TimeZone.getTimeZone("UTC"));
             }
             else {
@@ -322,7 +321,6 @@ public class PhotoServiceImpl implements PhotoService {
         final AbstractFacet newestFacet = photoFacetFinderStrategy.findLatest(guestId, connector, objectType);
 
         if (oldestFacet != null && newestFacet != null) {
-            // TODO: Not sure if this is correct for time zones...
             return new TimeInterval(oldestFacet.start, newestFacet.start, TimeUnit.DAY, TimeZone.getTimeZone("UTC"));
         }
 
